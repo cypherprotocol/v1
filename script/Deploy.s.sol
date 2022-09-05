f20fe436f7dba1735250a48f586dbae0680db10c
Bradley Woolf <bradleymwoolf@gmail.com>
2022-09-04 21:26:08 -0700

    hopefully deploy test script passes now

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.15;
 
 import {Script} from 'forge-std/Script.sol';
 
-import {CypherVault} from "src/CypherVault.sol";
+import {CypherRegistry} from "src/CypherRegistry.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
 
   /// @notice The main script entrypoint
   /// @return greeter The deployed contract
-  function run() external returns (CypherVault cypherVault) {
+  function run() external returns (CypherRegistry cypherRegistry) {
     vm.startBroadcast();
-    cypherVault = new CypherVault();
+    cypherRegistry = new CypherRegistry();
     vm.stopBroadcast();
   }
 }
