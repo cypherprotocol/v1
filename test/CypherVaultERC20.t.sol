0c194fd68f3513e83014087b66a37a40d9a2817f
Bradley Woolf <bradleymwoolf@gmail.com>
2022-09-27 10:48:56 -0700

    idk im about to lose my shit

--- test/CypherVaultERC20.t.sol
+++ test/CypherVaultERC20.t.sol
@@ -108,9 +108,9 @@ contract CypherVaultTest is Test {
     uint deposit = 50;
     // mint here since setUp is not working
     token.mint(hacker, 100);
-
+    emit log_named_address("token address in test", address(token));
     emit log_named_uint("balanceOf(hacker)", MockERC20(token).balanceOf(hacker));
-    assertEq(MockERC20(token).balanceOf(hacker), 100);
+    assertEq(token.balanceOf(hacker), 100);
 
     MockERC20(token).approve(address(mockRari), deposit);
     mockRari.depositTokens(deposit);
