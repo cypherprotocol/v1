6464c3a4e703aea47c23cedfb0763ca6ef078aaa
Alan Le <underrated@gmail.com>
2022-09-29 13:33:26 -0700

    👷‍♂️ fix stack overflow

--- test/CypherVaultERC20.t.sol
+++ test/CypherVaultERC20.t.sol
@@ -85,16 +85,16 @@ contract CypherVaultERC20Test is Test {
 
     // deposit ERC20 as collateral (can be USDC), get ETH back
     function testHackToken() public {
-        assertEq(mockRari.getContractBalance(), 100 ether);
+        assertEq(address(mockRari).balance, 100 ether);
         startHoax(hacker, 1 ether);
         assertEq(hacker.balance, 1 ether);
 
         attackTokenContract = new AttackToken(address(token), payable(address(mockRari)));
 
-        uint256 deposit = 50;
+        uint256 deposit = 1 ether;
         // mint here since setUp is not working
-        token.mint(address(attackTokenContract), 100);
-        assertEq(token.balanceOf(address(attackTokenContract)), 100);
+        token.mint(address(attackTokenContract), 1 ether);
+        assertEq(token.balanceOf(address(attackTokenContract)), 1 ether);
 
         // mockRari.depositTokens(deposit);
 
@@ -103,8 +103,8 @@ contract CypherVaultERC20Test is Test {
         // 2. reentering on the borrow
         attackTokenContract.attackRari(deposit);
 
-        // expect hacker to have 100eth + the original 1eth
-        assertEq(address(attackTokenContract).balance, 101 ether);
+        // expect attack contract to have 100eth
+        assertEq(address(attackTokenContract).balance, 100 ether);
         vm.stopPrank();
     }
 }
