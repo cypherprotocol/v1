6598a13128e7435ea2510eea7d0f0e26cbab8a0b
Alan Le <underrated@gmail.com>
2022-10-06 14:28:37 -0700

    ✨ external protocol name getter

--- src/interfaces/ICypherProtocol.sol
+++ src/interfaces/ICypherProtocol.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.13;
 
 interface ICypherProtocol {
     function getArchitect() external view returns (address);
+
+    function getEscrow() external view returns (address);
+
+    function getProtocolName() external view returns (string memory);
 }
