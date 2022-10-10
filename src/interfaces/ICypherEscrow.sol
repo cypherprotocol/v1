4ddb3e02178bbd2ccd2a79fd37994a9685b81f25
Bradley Woolf <bradleymwoolf@gmail.com>
2022-10-10 16:53:21 -0700

    👷update interface

--- src/interfaces/ICypherEscrow.sol
+++ src/interfaces/ICypherEscrow.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.13;
 
 interface ICypherEscrow {
     function escrowTokens(
-        address from,
-        address to,
+        address origin,
+        address dst,
         address asset,
         uint256 amount
     ) external;
 
-    function escrowETH(address from, address to) external payable;
+    function escrowETH(address origin, address dst) external payable;
 }
