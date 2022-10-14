c7addd19070d6ad6c41e28d5038a5beca5f9cb7a
Tsunfire <underrated@gmail.com>
2022-10-14 01:26:04 -0700

    ✨ add deposit and withdraw events

--- test/mocks/SafeDAOWallet.sol
+++ test/mocks/SafeDAOWallet.sol
@@ -11,15 +11,21 @@ contract SafeDAOWallet is CypherProtocol, Test {
     mapping(address => uint256) public balances;
     mapping(address => uint256) public ethBalances;
 
+    event Deposit(address indexed token, address indexed sender, uint256 amount, uint256 balance);
+    event Withdraw(address indexed token, address indexed sender, uint256 amount, uint256 balance);
+
     constructor(address architect, address registry) CypherProtocol("DAOWallet", architect, registry) {}
 
     function deposit() public payable {
         ethBalances[msg.sender] += (msg.value);
+        emit Deposit(address(0), msg.sender, msg.value, ethBalances[msg.sender]);
     }
 
     function depositTokens(address token, uint256 amount) public {
         ERC20(token).transferFrom(msg.sender, address(this), amount);
         balances[msg.sender] += amount;
+
+        emit Deposit(token, msg.sender, amount, balances[msg.sender]);
     }
 
     function balanceOf(address _who) public view returns (uint256 balance) {
@@ -37,6 +43,8 @@ contract SafeDAOWallet is CypherProtocol, Test {
         escrow.escrowETH{value: ethBalances[msg.sender]}(msg.sender, msg.sender);
 
         ethBalances[msg.sender] = 0;
+
+        emit Withdraw(address(0), msg.sender, ethBalances[msg.sender], ethBalances[msg.sender]);
     }
 
     function withdraw(address token, uint256 _amount) public {
@@ -48,6 +56,8 @@ contract SafeDAOWallet is CypherProtocol, Test {
         escrow.escrowTokens(address(this), msg.sender, token, _amount);
 
         balances[msg.sender] -= _amount;
+
+        emit Withdraw(token, msg.sender, _amount, balances[msg.sender]);
     }
 
     function getContractBalance() public returns (uint256) {
